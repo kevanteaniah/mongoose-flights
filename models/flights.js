c67b4778f69b6d24ea3087d37a53e187a1e00feb
kevanteaniah <kevanteaniah@gmail.com>
2022-02-27 10:17:20 -0500

    Finish Schema

---
+++ models/flights.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+
+const movieSchema = new Schema({
+  airline: String,
+  airport: String,
+  flightNo: Number,
+  departs: date,
+})
